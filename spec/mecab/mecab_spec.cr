eba4846cd5138f773413f382c1c3440584894a1d
Hisashi Morita <hisashim@workbook.org>
2022-11-23 18:18:08 +0900

    Apply crystal tool format using Crystal 1.6

--- spec/mecab/mecab_spec.cr
+++ spec/mecab/mecab_spec.cr
@@ -1,12 +1,12 @@
 require "../spec_helper"
 
-def ipadic()
-  return `mecab-config --dicdir`.chomp() + "/ipadic"
+def ipadic
+  return `mecab-config --dicdir`.chomp + "/ipadic"
 end
 
 def initialize_mecab(arg)
-  model   = LibMeCab.mecab_model_new2("-d #{ipadic()} #{arg}")
-  tagger  = LibMeCab.mecab_model_new_tagger(model)
+  model = LibMeCab.mecab_model_new2("-d #{ipadic} #{arg}")
+  tagger = LibMeCab.mecab_model_new_tagger(model)
   lattice = LibMeCab.mecab_model_new_lattice(model)
 
   return model, tagger, lattice
@@ -33,7 +33,7 @@ describe "lib_mecab", "#general" do
     model, tagger, lattice = initialize_mecab("")
 
     info = LibMeCab.mecab_dictionary_info(tagger)
-    String.new(info.value.filename).should eq("#{ipadic()}/sys.dic")
+    String.new(info.value.filename).should eq("#{ipadic}/sys.dic")
     String.new(info.value.charset).should match(/utf-?8/i)
 
     finalize_mecab(model, tagger, lattice)
@@ -52,7 +52,7 @@ describe "lib_mecab", "#parse" do
     result_ptr = LibMeCab.mecab_lattice_tostr(lattice)
     result = String.new(result_ptr)
 
-    result = result.rstrip()
+    result = result.rstrip
     result.should eq(expected)
 
     finalize_mecab(model, tagger, lattice)
@@ -61,13 +61,13 @@ describe "lib_mecab", "#parse" do
   it "analyzes a sentence" do
     sentence = "今日もしないとね。"
     expected = [
-      [ "今日", "名詞,副詞可能,*,*,*,*,今日,キョウ,キョー" ],
-      [ "も",   "助詞,係助詞,*,*,*,*,も,モ,モ" ],
-      [ "し",   "動詞,自立,*,*,サ変・スル,未然形,する,シ,シ" ],
-      [ "ない", "助動詞,*,*,*,特殊・ナイ,基本形,ない,ナイ,ナイ" ],
-      [ "と",   "助詞,接続助詞,*,*,*,*,と,ト,ト" ],
-      [ "ね",   "助詞,終助詞,*,*,*,*,ね,ネ,ネ" ],
-      [ "。",   "記号,句点,*,*,*,*,。,。,。" ]
+      ["今日", "名詞,副詞可能,*,*,*,*,今日,キョウ,キョー"],
+      ["も", "助詞,係助詞,*,*,*,*,も,モ,モ"],
+      ["し", "動詞,自立,*,*,サ変・スル,未然形,する,シ,シ"],
+      ["ない", "助動詞,*,*,*,特殊・ナイ,基本形,ない,ナイ,ナイ"],
+      ["と", "助詞,接続助詞,*,*,*,*,と,ト,ト"],
+      ["ね", "助詞,終助詞,*,*,*,*,ね,ネ,ネ"],
+      ["。", "記号,句点,*,*,*,*,。,。,。"],
     ]
 
     model, tagger, lattice = initialize_mecab("")
@@ -80,7 +80,7 @@ describe "lib_mecab", "#parse" do
     i = 0
     while (!node_ptr.null?)
       if (node_ptr.value.stat == LibMeCab::MECAB_NOR_NODE)
-        surface = String.new(node_ptr.value.surface.to_slice(node_ptr.value.length.to_i32()))
+        surface = String.new(node_ptr.value.surface.to_slice(node_ptr.value.length.to_i32))
         feature = String.new(node_ptr.value.feature)
 
         surface.should eq(expected[i][0])
